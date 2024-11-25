dc1fb85aeae828ce73b0bdf1ab7445cc199c9f42
omnisudo <omnisudo@gmail.com>
2024-11-25 07:20:41 -0600

    I tried my best, but in the end, it didn't even matter...

--- SkillQuest.Client.Engine/CL.cs
+++
@@ -1,9 +0,0 @@
-﻿using Silk.NET.Core.Native;
-
-namespace SkillQuest.Client.Engine;
-
-public class State {
-    public static State CL { get; } = new State();
-
-    public NativeAPI GraphicsAPI { get; set; }
-}
