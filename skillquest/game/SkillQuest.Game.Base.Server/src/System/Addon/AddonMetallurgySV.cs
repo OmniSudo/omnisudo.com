c680734536c8c9a1cdd2fed05b9163d8440abc86
omnisudo <omnisudo@gmail.com>
2024-12-10 14:33:07 -0600

    Fix addon inheritance

--- skillquest/game/SkillQuest.Game.Base.Server/src/System/Addon/AddonMetallurgySV.cs
+++ skillquest/game/SkillQuest.Game.Base.Server/src/System/Addon/AddonMetallurgySV.cs
@@ -3,7 +3,7 @@ using SkillQuest.API;
 
 namespace SkillQuest.Addon.Base.Server.Doohickey.Addon;
 
-public class AddonMetallurgySV : AddonMiningSH {
+public class AddonMetallurgySV : AddonMetallurgySH {
     public override Uri Uri { get; set; } = new Uri("sv://addon.skill.quest/metallurgy");
 
     public AddonMetallurgySV(){
--- skillquest/game/SkillQuest.Game.Base.Shared/src/System/Addon/AddonMiningSH.cs
+++ skillquest/game/SkillQuest.Game.Base.Shared/src/System/Addon/AddonMiningSH.cs
@@ -10,4 +10,16 @@ public class AddonMiningSH : global::SkillQuest.Shared.Engine.Addon{
     
     public override string Description { get; } = "Adds mining to the game";
 
+    public AddonMiningSH(){
+        Mounted += OnMounted;
+        Unmounted += OnUnmounted;
+    }
+
+    void OnMounted(IAddon addon, IApplication? application){
+        SH.Ledger.Components.LoadFromXmlFile( "game/SkillQuest.Game.Base.Shared/assets/Component/Item/Mining/Ore.xml" );
+        SH.Ledger.Items.LoadFromXmlFile( "game/SkillQuest.Game.Base.Shared/assets/Entity/Item/Mining/Coal.xml" );
+    }
+
+    void OnUnmounted(IAddon addon, IApplication? application){
+    }
 }
